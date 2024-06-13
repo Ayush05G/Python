64f439a367d351e47fa53da034d2dffcb3b316bc
Ayush Gupta <ayush2425.rk@gmail.com>
2024-06-14 02:28:52 +0530

    Add files via upload

---
+++ vector2D.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+bool search2DArray(vector<vector<int> > arr, int target){
+  int rowSize = arr.size();
+  int colSize = arr[0].size();
+
+  for(int i=0; i<rowSize; i++) {
+    for(int j=0; j<colSize; j++) {
+
+      if(arr[i][j] == target ){
+        return true;
+      }
+
+    }
+  }
+  return false;
+}
+
+int main() {
+  vector<vector<int> > arr(4, vector<int>(3,0));
+  int rowSize = arr.size();
+  int colSize = arr[0].size();
+  
+  for(int i=0; i<rowSize; i++) {
+    for(int j=0; j<colSize; j++) {
+      cin >> arr[i][j];
+    }
+  }
+
+  int target = 40;
+
+  bool ans = search2DArray(arr,target);
+  cout << "ans: " << ans << endl;
+
+
+  return 0;
+}
