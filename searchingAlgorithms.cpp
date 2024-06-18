3f5b6f6d485fc5d93bd64c4eb9c416846564afce
Ayush Gupta <ayush2425.rk@gmail.com>
2024-06-19 01:15:51 +0530

    Add files via upload

---
+++ searchingAlgorithms.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+
+int main() {
+
+    
+
+
+
+    //min/max in a range
+    // vector<int> arr;
+    // arr.push_back(10);
+    // arr.push_back(20);
+    // arr.push_back(30);
+    // arr.push_back(40);
+    // arr.push_back(50);
+
+    // auto it = max_element(arr.begin(), arr.end());
+    // cout << *it << endl;
+
+    // auto it = min_element(arr.begin(), arr.end());
+    // cout << *it << endl;
+
+
+
+    //2 number -> compare
+    // int a = 10;
+    // int b = 15;
+    // cout << max(a,b) << endl;
+    // cout << min(a,b) << endl;
+
+
+
+    // vector<int> arr;
+    // arr.push_back(10);
+    // arr.push_back(20);
+    // arr.push_back(30);
+    // arr.push_back(40);
+    // arr.push_back(50);
+
+    // auto it = upper_bound(arr.begin(), arr.end(), 40); 
+    // cout << *it << endl;
+    // auto it = lower_bound(arr.begin(), arr.end(), 35);
+    // cout << *it << endl;
+
+    // int target = 400;
+    // bool it = binary_search(arr.begin(), arr.end(), target);
+    // cout << it << endl;
+
+
+
+
+    return 0;
+}
