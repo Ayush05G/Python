cb45f960955d1c25f38741e0e66ef8512048f91d
Ayush Gupta <ayush2425.rk@gmail.com>
2024-06-21 03:26:03 +0530

    Add files via upload

---
+++ quickComilerCode.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+
+  int arr[] = {10,20,30,40,50,60,70,80,90};
+  int n = 9;
+  int target = 77;
+
+  bool ans = binary_search(arr, arr+n, target);
+
+  cout << ans << endl;
+
+
+  return 0;
+}
