cc46991d63c3754f08dd47c5cd7f0f8ab2d940ce
Ayush Gupta <ayush2425.rk@gmail.com>
2024-06-22 17:58:59 +0530

    Added Year 1 Python files

---
+++ cph1.py
@@ -0,0 +1,6 @@
+x=int(input("Total time in minutes: "))
+c=x%60
+z=x//60
+a=z//24
+b=z%24
+print(a,":",b,":",c)
---
+++ cph2.py
@@ -0,0 +1,9 @@
+x=int(input("Enter Tension:"))
+y=int(input("Enter mass:"))
+z=int(input("Enter length:"))
+a=int(x*z)
+b=a/y
+c=b**0.5
+d=c/2
+e=d/z
+print(e)
